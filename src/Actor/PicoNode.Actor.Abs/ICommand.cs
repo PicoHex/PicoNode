16fb0c1fdaafcd6f2a0bd27662bbdd80a8aa1186
Mutudu <aeondxf@live.com>
2026-07-18 11:16:16 +0800

    refactor: move Actor module to PicoActor repository
    - Remove src/Actor/ (PicoNode.Actor.Abs + PicoNode.Actor)
    - Remove tests/PicoNode.Actor.Tests and tests/PicoNode.Actor.Abs.Tests
    - Update PicoNode.slnx to remove Actor project entries
    - Remove PicoNode.Actor/Abs from Directory.Packages.props

--- src/Actor/PicoNode.Actor.Abs/ICommand.cs
+++
@@ -1,6 +0,0 @@
-namespace PicoNode.Actor.Abs;
-
-/// <summary>
-/// Marker interface. All commands sent to an actor must implement this.
-/// </summary>
-public interface ICommand { }
--- src/Actor/PicoNode.Actor/GlobalUsings.cs
+++
@@ -1,6 +0,0 @@
-global using System.Collections.Concurrent;
-global using PicoDI;
-global using PicoDI.Abs;
-global using PicoLog.Abs;
-global using PicoNode.Actor.Abs;
-global using ActorBase = PicoNode.Actor.Abs.Actor;
